ae31b4cccf69d1eb2f5925065f14d5c9082b16e2
amos-kibet <kibetamos97@gmail.com>
2021-05-01 14:04:18 +0300

    Rename NoArgConstructor.java to NoArgConstructor1.java

---
+++ NoArgConstructor1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package constructors;
+
+/**
+ * @author Amos
+ * Java Program to create and call a default constructor
+ */
+public class NoArgConstructor1 {
+	//creating a default constructor 
+	public NoArgConstructor1() {
+		System.out.println("A no-arguments Constructor is created");
+}
+	//main method 
+	public static void main(String[] args) {
+		NoArgConstructor1 con1 = new NoArgConstructor1();
+	}
+}
