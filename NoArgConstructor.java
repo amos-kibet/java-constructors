c43915e152869bbf5465f59b3dd686b4d2f04c20
amos-kibet <kibetamos97@gmail.com>
2021-05-01 13:48:45 +0300

    No-arguments constructor

---
+++ NoArgConstructor.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package constructors;
+
+/**
+ * @author Amos
+ * Java Program to create and call a default constructor
+ */
+public class NoArgConstructor {
+	//creating a default constructor 
+	public NoArgConstructor() {
+		System.out.println("A no-arguments Constructor is created");
+}
+	//main method 
+	public static void main(String[] args) {
+		NoArgConstructor con1 = new NoArgConstructor();
+	}
+}
